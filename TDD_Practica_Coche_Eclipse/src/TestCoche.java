9486d302e4584b2b00183b2548580169862c8c8d
Bufon <edelacotera@gmail.com>
2023-03-30 23:56:56 +0200

    añadidos los nuevos nombres de los métodos

--- TDD_Practica_Coche_Eclipse/src/TestCoche.java
+++ TDD_Practica_Coche_Eclipse/src/TestCoche.java
@@ -5,29 +5,30 @@ import org.junit.jupiter.api.Test;
 class TestCoche {
 
 	 @Test
-	    public void test_al_crear_un_coche_su_velocidad_es_cero(){
+	    public void eduardoDelaCoteraMartinez_test_al_crear_un_coche_su_velocidad_es_cero(){
 	        Coche nuevoCoche=new Coche();
 	        Assertions.assertEquals(0,nuevoCoche.velocidad);
 	    }
 	    @Test
-	    public void test_al_acelerar_un_coche_su_velocidad_aumenta(){
+	    public void eduardoDelaCoteraMartinez_test_al_acelerar_un_coche_su_velocidad_aumenta(){
 	        Coche nuevoCoche=new Coche();
 	        nuevoCoche.acelerar(30);
 	        Assertions.assertEquals(30,nuevoCoche.velocidad);
 	    }
 	    @Test
-	    public void test_al_descelerar_un_coche_su_velocidad_disminuye(){
+	    public void eduardoDelaCoteraMartinez_test_al_descelerar_un_coche_su_velocidad_disminuye(){
 	        Coche nuevoCoche=new Coche();
 	        nuevoCoche.velocidad=50;
 	        nuevoCoche.decelerar(20);
 	        Assertions.assertEquals(30,nuevoCoche.velocidad);
 	    }
 	    @Test
-	    public void test_al_descelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
+	    public void eduardoDelaCoteraMartinez_test_al_descelerar_un_coche_su_velocidad_no_puede_ser_menor_que_cero(){
 	        Coche nuevoCoche=new Coche();
 	        nuevoCoche.velocidad=50;
 	        nuevoCoche.decelerar(80);
 	        Assertions.assertEquals(0,nuevoCoche.velocidad);
 	    }
+	    //Añadido los nuevos nombres de los métodos
 
 }
